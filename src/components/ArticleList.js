9a73a51a3888669882c5040494416e693517ad3e
Igor_Sysoev <Igor_Sysoev@epam.com>
2018-02-01 15:58:05 +0300

    add article loading

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -5,43 +5,47 @@ import accordion from '../decorators/accordion'
 import {connect} from 'react-redux'
 import {filtratedArticlesSelector} from '../selectors'
 import {loadAllArticles} from "../AC";
+import Loader from './Loader'
 
 class ArticleList extends Component {
-    static propTypes = {
-        //from connect
-        articles: PropTypes.array.isRequired,
-        //from accordion
-        openItemId: PropTypes.string,
-        toggleOpenItem: PropTypes.func.isRequired
-    }
+  static propTypes = {
+    //from connect
+    articles: PropTypes.array.isRequired,
+    //from accordion
+    openItemId: PropTypes.string,
+    toggleOpenItem: PropTypes.func.isRequired
+  }
 
-    componentDidMount() {
-        this.props.loadAllArticles()
-    }
+  componentDidMount() {
+    const {loaded, loading, loadAllArticles} = this.props
+    if(!loaded || !loading) loadAllArticles()
+  }
 
 
+  render() {
+    console.log('-----', 'update article list')
+    const {articles, openItemId, toggleOpenItem, loading} = this.props
+    if(loading) return <Loader/>
+    const articleElements = articles.map(article => <li key={article.id}>
+      <Article
+        article={article}
+        isOpen={article.id === openItemId}
+        toggleOpen={toggleOpenItem(article.id)}
+      />
+    </li>)
 
-    render() {
-        console.log('-----', 'update article list')
-        const { articles, openItemId, toggleOpenItem } = this.props
-        const articleElements = articles.map(article => <li key={article.id}>
-            <Article
-                article = {article}
-                isOpen = {article.id === openItemId}
-                toggleOpen = {toggleOpenItem(article.id)}
-            />
-        </li>)
-
-        return (
-            <ul>
-                {articleElements}
-            </ul>
-        )
-    }
+    return (
+      <ul>
+        {articleElements}
+      </ul>
+    )
+  }
 }
 
 export default connect(state => {
-    return {
-        articles: filtratedArticlesSelector(state)
-    }
+  return {
+    articles: filtratedArticlesSelector(state),
+    loading: state.articles.loading,
+    loaded: state.articles.loaded
+  }
 }, {loadAllArticles})(accordion(ArticleList))
--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
-import {Map} from 'immutable'
+import {OrderedMap, Map} from 'immutable'
 
 export function arrToMap(arr, DataRecord = Map) {
-  return arr.reduce((acc, item) => acc.set(item.id, new DataRecord(item)), new Map({}))
+  return arr.reduce((acc, item) => acc.set(item.id, new DataRecord(item)), new OrderedMap({}))
 }
 
 export function mapToArr(obj) {
